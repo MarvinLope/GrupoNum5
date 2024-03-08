c9b75683b5b666c7c749f4cc24a646d964c88b1e
MarvinLope <mrlopez2@uth.hn>
2024-03-07 23:13:04 -0600

    Clase deportistad

--- Grupo5/src/main/java/com/mycompany/grupo5/Persona.java
+++ Grupo5/src/main/java/com/mycompany/grupo5/Persona.java
@@ -8,6 +8,33 @@ package com.mycompany.grupo5;
  *
  * @author USUARIO
  */
-public class Persona {
+public abstract class  Persona {
+    private String Nombre;
+    private int Edad;
+    public Persona(){
+        //Constructor sin parametros
+    }
+    
+    //Metodos para establecer y obtener los atributos de la clase
+    public void setNombre(String Nombre){
+        this.Nombre=Nombre;
+    }
+    
+    public String getNombre(){
+        return this.Nombre;
+    }
+     public void getEdad(String Nombre){
+        this.Nombre=Nombre;
+    }
+    
+    public String getEdad(){
+        return this.Nombre;
+    }
+    //Metodo para establecer color
+    public void establecerColor(){
+        System.out.println("Color: " + Color);
+    }
+    //Metodo Abstracto de dibujar
+    abstract String Profesion();
     
 }
