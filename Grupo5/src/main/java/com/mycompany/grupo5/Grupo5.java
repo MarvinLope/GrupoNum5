e86bf4c639f075876e7226ca243e5a59d8ace2f9
MarvinLope <mrlopez2@uth.hn>
2024-03-07 22:32:11 -0600

    Clase deportistad

---
+++ Grupo5/src/main/java/com/mycompany/grupo5/Grupo5.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.grupo5;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class Grupo5 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
